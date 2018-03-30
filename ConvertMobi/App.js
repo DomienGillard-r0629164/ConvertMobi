7ac7ed87a03c04675023c315a789d57591695c0c
WV <willem.verstappen@student.ucll.be>
2018-03-30 08:29:50 +0200

    minor layout test

--- ConvertMobi/App.js
+++ ConvertMobi/App.js
@@ -1,23 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, SectionList, Text, View, Button, Alert } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class Converter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { leftValue: 1, rightValue: 1};
+    }
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      /*
+      <View>
+        <View style={styles.default}>
+          <Converter text={"test"}>
+          </Converter>
+        </View>
+        */
+        <View style={styles.leftward}>
+          <Button
+          title="metre"
+          color="#000000"
+          onPress={panic}
+          />
+          <Button
+          title="yard"
+          color="#000000"
+          onPress={panic}
+          />
+        </View>
+        
+      //</View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  default: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  leftward: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+  }
 });
+
+function convert() {
+        return leftValue / rightValue;
+}
+
+function panic() {
+    Alert.alert(
+        'PANIC!!!',
+        "Don't panic too hard though. You managed an alert, at least."
+    )
+}
