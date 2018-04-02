d1e90b0370db67fbcd9d39888661dc02c43c4280
DomienGillard-R0629164 <domien.gillard@student.ucll.be>
2018-04-02 13:58:59 +0200

    mass converter werkt

--- ConvertMobi/App.js
+++ ConvertMobi/App.js
@@ -51,7 +51,27 @@ export class MassConverter extends React.Component {
   constructor(props)
   {
     super(props);
-    this.state = { leftValue: 1, rightValue: 1, leftUnit: "kg", rightUnit: "kg" };
+    this.state = { /*leftValue: 1,*/ rightValue: 1, result: 0, leftUnit: "kg", rightUnit: "kg", units: [
+      ["carats (metric)", "CD", 0.0002],
+      ["cental", "", 45.359237],
+      ["decagrams", "dag", 0.01],
+      ["femtograms", "fg", 1e-18],
+      ["grains", "gr", 0.00006479891],
+      ["grams", "g", 1e-3],
+      ["hectograms", "hg", 0.1],
+      ["hundredweights", "cwt", 50.80234544],
+      ["kilograms", "kg", 1],
+      ["kilotonnes", "kt", 1e6],
+      ["megatonnes", "Mt", 1e9],
+      ["micrograms", "Âµg", 1e-9],
+      ["milligrams", "mg", 1e-6],
+      ["nanograms", "ng", 1e-12],
+      ["ounces (US & UK)", "oz", 0.028349523125],
+      ["pounds (US & UK)", "lbs", 0.45359237],
+      ["stones", "st", 6.35029318],
+      ["tonnes (metric)", "t", 1000]
+      ]
+    }
   }
 
   render() {
@@ -61,7 +81,7 @@ export class MassConverter extends React.Component {
           <Button title="Distance Converter" onPress={() => this.goToDistanceConverter()} />
         </View>
         <View>
-          <TextInput onChangeText={(leftValue) => this.setState(leftValue)}/>
+          <TextInput onChangeText={(leftValue) => this.setState({leftValue})} keyboardType='numeric'/>
           <Picker selectedValue={this.state.leftUnit} onValueChange={(itemValue, itemIndex) => this.setState({leftUnit: itemValue})}>
             <Picker.Item label="Kilogram" value="kg"/>
             <Picker.Item label="Pound" value="lbs"/>
@@ -70,10 +90,25 @@ export class MassConverter extends React.Component {
             <Picker.Item label="Kilogram" value="kg"/>
             <Picker.Item label="Pound" value="lbs"/>
           </Picker>
-          <Text>{this.state.rightValue}</Text>
+          <Text>{this.state.result}</Text>
+          <Button title="Convert" onPress={() => this.convert()} />
         </View>
       </View>
     );
+  }
+
+  convert() {
+    var toVal = 0;
+    var fromVal = 0;
+    for(var i = 0; i < this.state.units.length; i++){
+      if(this.state.units[i][1] == this.state.rightUnit){
+        toVal = parseFloat(this.state.units[i][2]);
+      }
+      if(this.state.units[i][1] == this.state.leftUnit){
+        fromVal = parseFloat(this.state.units[i][2]);
+      }
+    }
+    this.setState({result: parseFloat((this.state.leftValue * fromVal) / toVal)});
   }
 
   goToDistanceConverter(){
